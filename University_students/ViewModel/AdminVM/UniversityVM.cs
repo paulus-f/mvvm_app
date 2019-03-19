cfa2d1f6d1ce1ce235926d5468142c54687c18f6
Pavel <fedorovich2012bel@gmail.com>
2019-03-20 02:13:14 +0300

    Add view for university in admin panel

---
+++ University_students/ViewModel/AdminVM/UniversityVM.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
+using System.ComponentModel;
+using System.Windows;
+using System.Runtime.CompilerServices;
+using University_students.Models;
+
+namespace University_students.ViewModel.AdminVM
+{
+    public class UniversityVM : ViewModelBase, INotifyPropertyChanged
+    {
+        USDbContext db;
+
+        public UniversityVM()
+        {
+            db = new USDbContext();
+        }
+        
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+    }
+}
+
